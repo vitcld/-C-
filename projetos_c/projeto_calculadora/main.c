414f64dd8ca243d10ef6813a4e1a4e756dd8a955
Victor Claudino <86791443+vitcld@users.noreply.github.com>
2024-05-27 21:50:00 -0300

    Update main.c

--- projetos_c/projeto_calculadora/main.c
+++ projetos_c/projeto_calculadora/main.c
@@ -9,7 +9,7 @@ void operacoes_basicas() {
     printf("Digite o primeiro número: ");
     scanf("%lf", &num1);
     printf("Digite a operação (+, -, *, /): ");
-    scanf(" %c", &operacao);
+    scanf(" %c", &operacao);  // Note the space before %c to consume any leftover newline character
     printf("Digite o segundo número: ");
     scanf("%lf", &num2);
 
@@ -33,6 +33,27 @@ void operacoes_basicas() {
         default:
             printf("Operação inválida!\n");
             break;
+    }
+}
+
+// Função para resolver equação de primeiro grau
+void equacao_primeiro_grau() {
+    double a, b;
+    printf("Para a equação ax + b = 0\n");
+    printf("Digite o valor de a: ");
+    scanf("%lf", &a);
+    printf("Digite o valor de b: ");
+    scanf("%lf", &b);
+
+    if (a != 0) {
+        double x = -b / a;
+        printf("A solução é: x = %.2lf\n", x);
+    } else {
+        if (b == 0) {
+            printf("A equação tem infinitas soluções.\n");
+        } else {
+            printf("A equação não tem solução.\n");
+        }
     }
 }
 
@@ -72,14 +93,75 @@ void equacao_segundo_grau() {
     }
 }
 
+// Função para calcular seno, cosseno e tangente
+void trigonometria() {
+    double angulo;
+    printf("Digite o ângulo em graus: ");
+    scanf("%lf", &angulo);
+
+    // Convertendo o ângulo de graus para radianos
+    double radiano = angulo * (M_PI / 180.0);
+
+    // Calculando seno, cosseno e tangente
+    double seno = sin(radiano);
+    double cosseno = cos(radiano);
+    double tangente = tan(radiano);
+
+    printf("Seno: %.2lf\n", seno);
+    printf("Cosseno: %.2lf\n", cosseno);
+    printf("Tangente: %.2lf\n", tangente);
+}
+
+// Função para calcular várias funções matemáticas
+void calcular_funcoes() {
+    int opcao;
+    double valor;
+
+    printf("\nEscolha a função que deseja calcular:\n");
+    printf("1. Exponencial (e^x)\n");
+    printf("2. Logaritmo natural (ln x)\n");
+    printf("3. Logaritmo base 10 (log10 x)\n");
+    printf("Opção: ");
+    scanf("%d", &opcao);
+
+    printf("Digite o valor de x: ");
+    scanf("%lf", &valor);
+
+    switch (opcao) {
+        case 1:
+            printf("Exponencial: %.2lf\n", exp(valor));
+            break;
+        case 2:
+            if (valor > 0) {
+                printf("Logaritmo natural: %.2lf\n", log(valor));
+            } else {
+                printf("Erro: Logaritmo natural não definido para valores <= 0.\n");
+            }
+            break;
+        case 3:
+            if (valor > 0) {
+                printf("Logaritmo base 10: %.2lf\n", log10(valor));
+            } else {
+                printf("Erro: Logaritmo base 10 não definido para valores <= 0.\n");
+            }
+            break;
+        default:
+            printf("Opção inválida!\n");
+            break;
+    }
+}
+
 int main() {
     int escolha;
 
     do {
         printf("\nEscolha uma opção:\n");
         printf("1. Operações básicas\n");
-        printf("2. Equação de segundo grau\n");
-        printf("3. Sair\n");
+        printf("2. Equação de primeiro grau\n");
+        printf("3. Equação de segundo grau\n");
+        printf("4. Trigonometria\n");
+        printf("5. Funções\n");
+        printf("6. Sair\n");
         printf("Opção: ");
         scanf("%d", &escolha);
 
@@ -88,16 +170,25 @@ int main() {
                 operacoes_basicas();
                 break;
             case 2:
-                equacao_segundo_grau();
+                equacao_primeiro_grau();
                 break;
             case 3:
+                equacao_segundo_grau();
+                break;
+            case 4:
+                trigonometria();
+                break;
+            case 5:
+                calcular_funcoes();
+                break;
+            case 6:
                 printf("Saindo...\n");
                 break;
             default:
                 printf("Opção inválida! Tente novamente.\n");
                 break;
         }
-    } while (escolha != 3);
+    } while (escolha != 6);
 
     return 0;
 }
